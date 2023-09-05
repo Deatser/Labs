e16133c1ee0114617dbff6578c1994e29111b230
Deatser <selezneff.sergej2011@yandex.ru>
2023-09-06 00:15:56 +0300

    1

--- src/ru/mirea/lab1/Example.java
+++ src/ru/mirea/lab1/Example.java
@@ -1,17 +1,66 @@
 package ru.mirea.lab1;
+import java.util.Scanner;
 
 public class Example {
-    public static void main(String[] args) {
-    // Задача 1: инициализация массива, сумма и среднее арифметическое
-    int[] arr = {1, 2, 3, 4, 5};
-    int sum = 0;
-    for (int i = 0; i < arr.length; i++) {
-        sum += arr[i];
+    public static int Factorial(int f) {
+        int result = 1;
+        for (int i = 1; i <= f; i++) {
+            result = result * i;
+        }
+        return result;
     }
-    double average = (double) sum / arr.length;
+    public static void main(String[] args) {
+    // Задача 1: Сумма и среднее арифметическое элементов целочисленного массива
+    int[] mas = {1, 2, 3, 4, 5};
+    int sum = 0;
+    for (int ma : mas) {
+        sum += ma;
+    }
+    double average = (double) sum / mas.length;
     System.out.println("Сумма элементов: " + sum);
     System.out.println("Среднее арифметическое: " + average);
 
+    // Задача 2: ввод значений целочисленного массива, нахождение суммы, минимального и максимального значений
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Введите размер массива: ");
+    int size = sc.nextInt();
+    int[] userMas = new int[size];
+    int userSum = 0;
+    int userMax = Integer.MIN_VALUE;
+    int userMin = Integer.MAX_VALUE;
+    int i = 0;
+    System.out.print("Введите элементы массива: \n");
+    do {
+        int elem = sc.nextInt();
+        userMas[i] = elem;
+        userSum += elem;
+        if (elem > userMax) {
+            userMax = elem;
+        }
+        if (elem < userMin) {
+            userMin = elem;
+        }
+        i++;
+    } while (i < size);
+    System.out.println("Сумма элементов: " + userSum);
+    System.out.println("Максимальное значение: " + userMax);
+    System.out.println("Минимальное значение: " + userMin);
 
+    // Задача 3: Вывод на экран аргументов командной строки в цикле for
+    System.out.println("Aргументы командной строки: " + args.length);
+    for (String arg : args) {
+        System.out.println(arg);
+
+    }
+    // Задача 4: Вывод первых 10 чисел гармонического ряда
+    System.out.println("Первые 10 чисел гармонического ряда: ");
+    for (i = 1; i <= 10; i++) {
+        System.out.printf("1/%d = %.3f%n", i, 1.0 / i);
+    }
+
+    // Задача 5: Вычисление факториала с помощью метода класса
+    System.out.println("Введите число для вычисления факториала: ");
+    int number = sc.nextInt();
+    System.out.println(Factorial(number));
     }
 }
